c4a27bed13468fb5b0042c54125c80661adeffd7
Daniela Parra <dyparra47@gmail.com>
2019-04-29 13:59:44 -0400

    Add schedule overview cell file

---
+++ When To Water/When To Water/New Group/SheduleOverviewCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SheduleOverviewCollectionViewCell.swift
+//  When To Water
+//
+//  Created by Daniela Parra on 4/29/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class SheduleOverviewCollectionViewCell: UICollectionViewCell {
+    
+    // Update cell's labels and indicator
+    private func updateViews() {
+        
+    }
+    
+    // MARK - Properties
+    
+    @IBOutlet weak var weekdayLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var indicatorView: UIView!
+}
