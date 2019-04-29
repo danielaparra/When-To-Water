68b7d1d51b5ea32bfd33f00cb56b454ecb622478
Daniela Parra <dyparra47@gmail.com>
2019-04-29 14:33:48 -0400

    Add schedule CVC header reusable view

---
+++ When To Water/When To Water/New Group/DailyScheduleHeaderCollectionReusableView.swift
@@ -0,0 +1,22 @@
+//
+//  DailyScheduleHeaderCollectionReusableView.swift
+//  When To Water
+//
+//  Created by Daniela Parra on 4/29/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class DailyScheduleHeaderCollectionReusableView: UICollectionReusableView {
+    
+    // Update weekday and date labels for section.
+    private func updateViews() {
+        
+    }
+    
+    // MARK - Properties
+    
+    @IBOutlet weak var weekdayLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
