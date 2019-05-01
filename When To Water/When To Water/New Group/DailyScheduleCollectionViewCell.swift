1e63bf6d79e75d80c6b5cf910457cda12e9de3f1
Daniela Parra <dyparra47@gmail.com>
2019-05-01 13:51:41 -0400

    Add daily schedule collection cell

---
+++ When To Water/When To Water/New Group/DailyScheduleCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DailyScheduleCollectionViewCell.swift
+//  When To Water
+//
+//  Created by Daniela Parra on 5/1/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class DailyScheduleCollectionViewCell: UICollectionViewCell {
+    
+    // MARK - Properties
+    
+    @IBOutlet weak var plantImageView: UIImageView!
+    @IBOutlet weak var plantNameLabel: UILabel!
+    @IBOutlet weak var plantLocationLabel: UILabel!
+    @IBOutlet weak var checkUncheckButton: UIButton!
+}
