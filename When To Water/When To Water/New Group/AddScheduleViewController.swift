d4b9224eb4c970ba6a4ff53677f08af4b7417f82
Daniela Parra <dyparra47@gmail.com>
2019-05-02 23:08:22 -0400

    Add add schedule VC

---
+++ When To Water/When To Water/New Group/AddScheduleViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddScheduleViewController.swift
+//  When To Water
+//
+//  Created by Daniela Parra on 5/2/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class AddScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK - Properties
+    @IBOutlet weak var plantImageView: UIImageView!
+    @IBOutlet weak var doesKnowLastWaterSwitch: UISwitch!
+    
+}
