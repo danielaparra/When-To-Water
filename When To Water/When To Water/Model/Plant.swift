7e53a962365ef49b9d813e34f436a6bdeae6d0c5
Daniela Parra <dyparra47@gmail.com>
2019-05-28 00:03:12 -0400

    Add VC fold folder and add Plant model

---
+++ When To Water/When To Water/Model/Plant.swift
@@ -0,0 +1,17 @@
+//
+//  Plant.swift
+//  When To Water
+//
+//  Created by Daniela Parra on 5/27/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import Foundation
+
+struct Plant {
+    let id: Int
+    let user_id: Int
+    var name: String
+    var description: String
+    var last_water: Date?
+}
